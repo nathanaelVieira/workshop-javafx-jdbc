041bc62b275f228025732c85bbce0ae72d1558a9
nathanael <vieira_nathanael@hotmail.com>
2022-06-27 15:39:19 -0300

    Main View

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
-module workshop {
+open module workshop {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
-	opens application to javafx.graphics, javafx.fxml;
+//	opens application to javafx.graphics, javafx.fxml;
 }
