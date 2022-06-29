79135c81ef9e2725e4a7072100efd490e9211555
nathanael <vieira_nathanael@hotmail.com>
2022-06-29 17:50:38 -0300

    DepartmentService/ module: javafx.base to entities

--- src/module-info.java
+++ src/module-info.java
@@ -2,8 +2,10 @@ module workshop {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 
 //	add modularização
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
+	opens model.entities to javafx.base;
 }
