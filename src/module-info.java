8a4f75ee7eb6b33f3a175c25ca8f1dda1d50458c
nathanael <vieira_nathanael@hotmail.com>
2022-06-29 22:00:00 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module workshop {
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.base;
+	requires java.sql;
 
 //	add modularização
 	opens application to javafx.graphics, javafx.fxml;
